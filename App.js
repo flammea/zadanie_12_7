79812544ffb2434e6d1fb3c85b45fb79de191f88
Beata Markunas <beata.markunas@gmail.com>
2018-05-03 09:00:51 +0200

    Change code from last module. Implement Ajax.

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
-
 // KOMUNIKACJA Z SERWEREM
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': 'X-Client-Id',
-  'X-Auth-Token': 'X-Auth-Token'
+  'X-Client-Id': '2811',
+  'X-Auth-Token': '306a09f5ad0da820c5fe081e025fd9e0'
 };
 
 $.ajaxSetup({
